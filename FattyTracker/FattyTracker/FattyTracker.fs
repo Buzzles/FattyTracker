a7dd44647375e51b1f136f74e69d24238c001d31
Ian Buswell <ianbuswell@etech.net>
2016-02-11 10:52:56 +0000

    Initial Commit of SLN and Project

---
+++ FattyTracker/FattyTracker/FattyTracker.fs
@@ -0,0 +1,4 @@
+﻿namespace FattyTracker
+
+type Class1() = 
+    member this.X = "F#"
---
+++ FattyTracker/FattyTracker/Script.fsx
@@ -0,0 +1,8 @@
+﻿// Learn more about F# at http://fsharp.org. See the 'F# Tutorial' project
+// for more guidance on F# programming.
+
+#load "FattyTracker.fs"
+open FattyTracker
+
+// Define your library scripting code here
+
