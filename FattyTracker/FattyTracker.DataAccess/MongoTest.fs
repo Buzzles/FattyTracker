6ba4c9aa96313da65fd63b5b9a452c9a757ffb2e
Ian Buswell <ianbuswell@etech.net>
2016-03-24 20:40:38 +0000

    Connection Settings/String example

--- FattyTracker/FattyTracker.DataAccess/MongoTest.fs
+++ FattyTracker/FattyTracker.DataAccess/MongoTest.fs
@@ -4,5 +4,11 @@ open MongoDB.Driver
 open MongoDB.Driver.Core
 open MongoDB.Bson
 
-let _client = new MongoClient();
+
+let mongoSettings = 
+    let connt = new MongoClientSettings()
+    connt.Server <- new MongoServerAddress("contString")
+    connt
+
+let _client = new MongoClient(mongoSettings);
 let _database = _client.GetDatabase("test");
