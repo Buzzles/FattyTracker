073c6f2e80d6fd3213bfea15777dfdc1c9b9a97f
Ian <ianbuswell@gmail.com>
2016-03-25 21:43:21 +0000

    Updating Mongo Settings to be in a more F# format

--- FattyTracker/FattyTracker.DataAccess/MongoTest.fs
+++ FattyTracker/FattyTracker.DataAccess/MongoTest.fs
@@ -8,15 +8,10 @@ let dockerIp = "192.168.99.100"
 let dockerPort = 32768
 
 let mongoSettings = 
-    let connt = new MongoClientSettings()
-    connt.Server <- new MongoServerAddress("contString")
-    connt
+    let clientSettings = new MongoClientSettings()
+    clientSettings.Server <- new MongoServerAddress(dockerIp, dockerPort)
+    clientSettings
 
 let _client = new MongoClient(mongoSettings);
-
-let settings = 
-    let x = new MongoServerAddress(dockerIp, dockerPort) 
-            |> _client.Settings.Server
-    _client.Settings.Server
 
 let _database = _client.GetDatabase("test");
