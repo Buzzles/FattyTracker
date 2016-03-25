b467a3f118f226ca1e047805b8697efd6034a7a9
Ian <ianbuswell@gmail.com>
2016-03-25 21:28:11 +0000

    Adding connection strings and mongo test file

--- FattyTracker/FattyTracker.DataAccess/MongoTest.fs
+++ FattyTracker/FattyTracker.DataAccess/MongoTest.fs
@@ -4,5 +4,14 @@ open MongoDB.Driver
 open MongoDB.Driver.Core
 open MongoDB.Bson
 
+let dockerIp = "192.168.99.100"
+let dockerPort = 32768
+
 let _client = new MongoClient();
+
+let settings = 
+    let x = new MongoServerAddress(dockerIp, dockerPort) 
+            |> _client.Settings.Server
+    _client.Settings.Server
+
 let _database = _client.GetDatabase("test");
